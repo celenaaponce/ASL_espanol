ac6dffcd57fc3fb00ca5af54b53b4ea97f7f7c56
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-07-10 19:26:20 -0700

    Update 10_test.py

--- streamlit_website/pages/10_test.py
+++ streamlit_website/pages/10_test.py
@@ -2,13 +2,7 @@ import pandas as pd
 import streamlit as st
 import io
 import dropbox
-DBX = dropbox.Dropbox(st.secrets["token"])
-_, res = DBX.files_download("/Themes2.csv")
 
-with io.BytesIO(res.content) as stream:
-    df = pd.read_csv(stream, index_col=0)
-
-st.write(df.head(5))
 # Read in data from the Google Sheet.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 @st.cache_data(ttl=600)
@@ -19,5 +13,29 @@ def load_data(sheets_url):
 df = load_data(st.secrets["public_gsheets_url"])
 
 
+import requests
 
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
+def download_file_from_google_drive(id, destination):
+    
+    URL = "https://drive.google.com/file/d/1ynYsJEwmJEiCqfDEbTzvBDvHWHKNZeLG/view?usp=drive_link"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
 
