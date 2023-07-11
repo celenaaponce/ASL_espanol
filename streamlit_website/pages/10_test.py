f315a0e097ab9d3afb84def390d6a1b21bc069b5
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-07-10 18:26:48 -0700

    Update 10_test.py

--- streamlit_website/pages/10_test.py
+++ streamlit_website/pages/10_test.py
@@ -14,9 +14,5 @@ df = load_data(st.secrets["public_gsheets_url"])
 for row in df.itertuples():
     st.write(f"{row.name} has a :{row.pet}:")
 
-chunksize = 10 ** 6
-with pd.read_csv('streamlit_website/Search List2.csv', chunksize=chunksize) as reader:
-    for chunk in reader:
-        process(chunk)
-
-st.write(reader.head(10))
+reader = pd.read_csv('streamlit_website/Search List2.csv', iterator=True)
+reader.get_chunk(100)
