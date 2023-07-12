0ac0a2da1916f9ccaa1e9a9687b58a5f46b3fe33
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-07-11 19:16:48 -0700

    Update 10_test.py

--- streamlit_website/pages/10_test.py
+++ streamlit_website/pages/10_test.py
@@ -20,8 +20,10 @@ def main():
     # Read the CSV file
     if st.button("Read CSV"):
         try:
-            df = pd.read_csv(output_file)
-            st.dataframe(df)
+            csv_length = 0    
+            for chunk in pd.read_csv(fileinput, names=['Palabra', 'Tema', 'Video', 'Imagen', 'Sinómino'], skiprows=skip, chunksize=10000):
+                csv_length += chunk.count()
+            st.write(csv_length )
         except FileNotFoundError:
             st.error("Please download the file first.")
 
