d2a6664806724803c2511ebac6a897192c92c950
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-07-17 21:44:36 -0700

    Update 10_test.py

--- streamlit_website/pages/10_test.py
+++ streamlit_website/pages/10_test.py
@@ -7,21 +7,25 @@ def download_csv(file_id, output_file):
     url = f'https://drive.google.com/uc?id={file_id}'
     gdown.download(url, output_file, quiet=False)
 def main():
+    placeholder = st.empty()
     content = """<p><a href='#' id='Link 1'>First link</a></p>
     <p><a href='#' id='Link 2'>Second link</a></p>
     <a href='#' id='Image 1'><img width='20%' src='https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=200'></a>
     <a href='#' id='Image 2'><img width='20%' src='https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=200'></a>
     """
     clicked = ""
-    st.header('Main')
-    clicked_main = click_detector(content)
+    with placeholder.container():
+        st.header('Main')
+        clicked_main = click_detector(content, key='main')
 
     if clicked_main == 'Image 1' or clicked == 'Image 1':
+        placeholder.empty()
         st.header('Second')
-        clicked = click_detector(content)
+        clicked = click_detector(content, key = 'second')
         st.write('here')
 
     elif clicked_main == 'Image 2' or clicked == 'Image 2':
+        placeholder.empty()
         st.header('Third')
         st.write('here2')
 if __name__ == "__main__":
