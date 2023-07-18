ff7167fc71194b3e4ecd42500f26d8df0e86763f
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-07-17 21:37:42 -0700

    Update 10_test.py

--- streamlit_website/pages/10_test.py
+++ streamlit_website/pages/10_test.py
@@ -6,28 +6,28 @@ def download_csv(file_id, output_file):
     url = f'https://drive.google.com/uc?id={file_id}'
     gdown.download(url, output_file, quiet=False)
 def main():
-    st.title("CSV File Reader")
+    content = """<p><a href='#' id='Link 1'>First link</a></p>
+    <p><a href='#' id='Link 2'>Second link</a></p>
+    <a href='#' id='Image 1'><img width='20%' src='https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=200'></a>
+    <a href='#' id='Image 2'><img width='20%' src='https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=200'></a>
+    """
+    clicked_main = ""
 
-    # Input file ID and output file name
-    file_id = st.text_input("Enter the file ID:")
-    output_file = "output.csv"
+    while clicked_main == "":
+        clicked_main = click_detector(content)
+        st.markdown(f"**{clicked} clicked**" if clicked != "" else "**No click**")
 
-    # Download the file
-    if st.button("Download"):
-        download_csv('1ynYsJEwmJEiCqfDEbTzvBDvHWHKNZeLG', 'Small Preview2.csv')
-        st.success("File downloaded successfully!")
+    if clicked_main == 'Image 1' or clicked == 'Image 1':
+        clicked = click_detector(content)
+        st.write('here')
 
-    # Read the CSV file
-    if st.button("Read CSV"):
-        try:
-            
-            csv_length = 0    
-            for chunk in pd.read_csv('Small Preview2.csv', names=['Palabra', 'Tema', 'Video', 'Imagen', 'Sinómino'], chunksize=10000, skiprows=1):
-                data = pd.DataFrame(chunk)
-                csv_length += chunk.count()
-            st.write(data.head(5))
-        except FileNotFoundError:
-            st.error("Please download the file first.")
-
+    elif clicked_main == 'Image 2' or clicked == 'Image 2':
+        st.write('here2')
 if __name__ == "__main__":
     main()
+import streamlit as st
+from st_click_detector import click_detector
+
+
+
+
