cc2d4d087be11b2251767c430844771b65042210
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-07-17 22:50:41 -0700

    Update 7_Diccionario_por_Letra.py

--- streamlit_website/pages/7_Diccionario_por_Letra.py
+++ streamlit_website/pages/7_Diccionario_por_Letra.py
@@ -131,9 +131,11 @@ def images(size):
          <a href='#' id='Image 27'>{other}</a>
          """
       if not st.session_state.load_images:
+        st.write('load', st.session_state.load_images)
         clicked = click_detector(content)
         st.write('adding images')
         st.session_state.load_images = True
+        st.write('load', st.session_state.load_images)
       return clicked
 
 def print_list(next_list):
@@ -153,11 +155,13 @@ if st.session_state.download == False:
   download_csv('1bii0vusXl-640sgVhRK2NVj8XCZtGgDx', 'Search List2.csv')
 
 if not st.session_state.load_images:
+   st.write('load in', st.session_state.load_images)
   #set up main page with images  
   clicked = images(10)
   set_start(clicked[6:])
   word_data = load_words()
   st.session_state.load_images = True
+  st.write('load in', st.session_state.load_images)
 
 #clicking logic
 if clicked[6:] == '27': 
