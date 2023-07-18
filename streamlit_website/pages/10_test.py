e4d3ac1d0e4f4cf29070a46d0b7cf37a45f0f5d9
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-07-17 23:42:37 -0700

    Update 10_test.py

--- streamlit_website/pages/10_test.py
+++ streamlit_website/pages/10_test.py
@@ -3,32 +3,29 @@ import pandas as pd
 import gdown
 from st_click_detector import click_detector
 
-
-
-def main():
-    placeholder = st.empty()
-    content = """<p><a href='#' id='Link 1'>First link</a></p>
-    <p><a href='#' id='Link 2'>Second link</a></p>
-    <a href='#' id='Image 1'><img width='20%' src='https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=200'></a>
-    <a href='#' id='Image 2'><img width='20%' src='https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=200'></a>
-    """
-    empty()
-    with placeholder.container():
-        clicked = click_detector(content, key='main')
-        
-    if clicked == 'Image 1':
-        st.header('Second')
-        st.write('here')
-
-    elif clicked == 'Image 2':
-        st.header('Third')
-        st.write('here2')
-
 def empty():
     placeholder.empty()
     sleep(0.01)
-if __name__ == "__main__":
-    main()
+
+placeholder = st.empty()
+content = """<p><a href='#' id='Link 1'>First link</a></p>
+<p><a href='#' id='Link 2'>Second link</a></p>
+<a href='#' id='Image 1'><img width='20%' src='https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=200'></a>
+<a href='#' id='Image 2'><img width='20%' src='https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=200'></a>
+"""
+empty()
+with placeholder.container():
+    clicked = click_detector(content, key='main')
+    
+if clicked == 'Image 1':
+    st.header('Second')
+    st.write('here')
+
+elif clicked == 'Image 2':
+    st.header('Third')
+    st.write('here2')
+
+
 
 
 
