3e00a5b5e6dfb2870eeb48b27f44966e051b3f56
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-07-19 12:00:28 -0700

    Update 10_test.py

--- streamlit_website/pages/10_test.py
+++ streamlit_website/pages/10_test.py
@@ -1,34 +1,20 @@
 import streamlit as st
-import pandas as pd
-import gdown
 from st_click_detector import click_detector
-from time import sleep
 
-def empty():
-    placeholder.empty()
-    sleep(0.01)
-
-placeholder = st.empty()
-content = """<p><a href='#' id='Link 1'>First link</a></p>
-<p><a href='#' id='Link 2'>Second link</a></p>
-<a href='#' id='Image 1'><img width='20%' src='https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=200'></a>
-<a href='#' id='Image 2'><img width='20%' src='https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=200'></a>
-"""
-empty()
-with placeholder.container():
+def render_content():
+    content = """
+    <a href='#' id='Image 1'><img width='20%' src='https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=200'></a>
+    <a href='#' id='Image 2'><img width='20%' src='https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=200'></a>
+    """
+    
     clicked = click_detector(content, key='main')
     
-if clicked == 'Image 1':
-    st.header('Second')
-    st.write('here')
+    if clicked == 'Image 1':
+        st.header('First')
+        st.write('here')
 
-elif clicked == 'Image 2':
-    st.header('Third')
-    st.write('here2')
+    elif clicked == 'Image 2':
+        st.header('Second')
+        st.write('there')
 
-
-
-
-
-
-
+render_content()
