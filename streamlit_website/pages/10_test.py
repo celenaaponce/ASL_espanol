3c3d6a6c491e14725df36d2eafe6306642b22222
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-07-23 19:17:14 -0700

    Update 10_test.py

--- streamlit_website/pages/10_test.py
+++ streamlit_website/pages/10_test.py
@@ -5,23 +5,41 @@ import streamlit.components.v1 as com
 
 def render_content(): 
     
-    st.title("Clickable Images with IDs")
+   com.html("""<!DOCTYPE html>
+<html>
+<head>
+    <title>Clickable Images with IDs</title>
+</head>
+<body>
+    <!-- Clickable Image 1 -->
+    <img src="https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=200" alt="Image 1" width="300" height="200" id="img1">
 
-    # Define a list of image URLs and their corresponding IDs
-    images = [
-        {"id": "img1", "url": "https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=200"},
-        {"id": "img2", "url": "https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=200"},
-        {"id": "img3", "url": "https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=200"}
-    ]
+    <!-- Clickable Image 2 -->
+    <img src="https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=200" alt="Image 2" width="300" height="200" id="img2">
 
-    # Display the images and make them clickable
-    for img in images:
-        image_url = img["url"]
-        image_id = img["id"]
-        st.write(f"Image ID: {image_id}")
-        st.image(image_url, use_column_width=True, caption=f"Image {image_id}", output_format="JPEG")
-        # Use st.button to create clickable buttons
-        if st.button(f"Click Image {image_id}"):
-            # When the button is clicked, display the image ID
-            st.write(f"You clicked Image {image_id}")
+    <!-- Clickable Image 3 -->
+    <img src="https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=200" alt="Image 3" width="300" height="200" id="img3">
+
+    <!-- Display the clicked image ID -->
+    <p id="clickedImageID"></p>
+
+    <script>
+        // Function to handle image click event
+        function handleImageClick(event) {
+            var clickedImage = event.target; // Get the clicked image element
+            var imageID = clickedImage.id;   // Get the ID of the clicked image
+            
+            // Update the "clickedImageID" element with the clicked image ID
+            document.getElementById('clickedImageID').innerText = "Clicked Image ID: " + imageID;
+        }
+
+        // Attach click event listeners to the images
+        var images = document.querySelectorAll('img');
+        images.forEach(function(image) {
+            image.addEventListener('click', handleImageClick);
+        });
+    </script>
+</body>
+</html>
+""")
 render_content()
